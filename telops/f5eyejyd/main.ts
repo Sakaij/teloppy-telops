39552d7562fbf5bbd80aa0948170f0673961cab7
Sakaij <sakai.github@gmail.com>
2022-03-17 02:14:31 +0900

    f5eyejydページ追加

---
+++ telops/f5eyejyd/main.ts
@@ -0,0 +1,41 @@
+
+import './style.scss';
+import fullHeight from 'plugins/fullHeight';
+import messageSplit from 'plugins/messageSplit';
+import TelopBase from 'utils/telopBase';
+import fontsizeAdjust from 'plugins/fontsizeAdjust';
+
+
+const timer = (time: number) => new Promise<void>((resolve) => setTimeout(resolve, time));
+
+class App extends TelopBase {
+	async ready() {
+		const container = document.querySelector<HTMLElement>('.full-height');
+		if (container) fullHeight(container);
+		const $pad = document.querySelector(".pad"),
+		$padBody = $pad?.querySelector('.pad_body');
+		if (!$pad || !$padBody) return;
+		const messageArray = messageSplit(this.decryptedMessage);
+		messageArray.forEach((v) => {
+			const $span = document.createElement('span');
+			$span.innerHTML = v;
+			$span.className = 'pad_body_part'
+			$padBody.appendChild($span);
+		});
+		fontsizeAdjust($padBody, $padBody.querySelectorAll('span'), true);
+
+		const partsArray = Array.from($padBody.querySelectorAll('.pad_body_part'));
+		await timer(500);
+		$pad.classList.add("pad--visible");
+		await partsArray.reduce(async (p: Promise<void>, e: Element) => {
+			await p;
+			e.classList.add("pad_body_part--visible");
+			await timer(250);
+			return p;
+		  }, timer(1200));
+		await timer(2000);
+		this.finish();
+	}
+}
+
+new App({telopId:"hhsditth",isLogoBlack:true}).init();

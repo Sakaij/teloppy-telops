22eaefecf81a1325014ac1f4b8bf62ba629b8571
Sakaij <sakai.github@gmail.com>
2022-05-15 03:10:14 +0900

    peb3gpju,v2day2cuページ追加

---
+++ telops/peb3gpju/main.ts
@@ -0,0 +1,36 @@
+
+import './style.scss';
+import fullHeight from 'plugins/fullHeight';
+import fontsizeAdjust from 'plugins/fontsizeAdjust';
+import messageSplit from 'plugins/messageSplit';
+import TelopBase from 'utils/telopBase';
+
+
+const timer = (time: number) => new Promise<void>((resolve) => setTimeout(resolve, time));
+
+class App extends TelopBase {
+	async ready() {
+		const container = document.querySelector<HTMLElement>('.full-height');
+		if (container) fullHeight(container);
+		const $catchMain = document.querySelector(".catch_main");
+		if (!$catchMain) return;
+		const messageArray = messageSplit(this.decryptedMessage);
+		messageArray.forEach((v) => {
+			const $span = document.createElement('span');
+			$span.innerHTML = v;
+			$span.className = 'catch_main_part'
+			$catchMain.appendChild($span);
+		});
+		const $parts = $catchMain.querySelectorAll('.catch_main_part');
+		const partsArray = Array.from($parts);
+		fontsizeAdjust($catchMain, $parts, true);
+		await timer(500);
+		partsArray.forEach((e: Element) => e.classList.add("catch_main_part--blink"));
+		await timer(1500);
+		partsArray.forEach((e: Element) => e.classList.remove("catch_main_part--blink"));
+		partsArray.forEach((e: Element) => e.classList.add("catch_main_part--active"));
+		this.finish();
+	}
+}
+
+new App({ telopId: "peb3gpju", isLogoBlack: true }).init();
---
+++ telops/v2day2cu/main.ts
@@ -0,0 +1,37 @@
+
+import './style.scss';
+import fullHeight from 'plugins/fullHeight';
+import fontsizeAdjust from 'plugins/fontsizeAdjust';
+import messageSplit from 'plugins/messageSplit';
+import TelopBase from 'utils/telopBase';
+
+
+const timer = (time: number) => new Promise<void>((resolve) => setTimeout(resolve, time));
+
+class App extends TelopBase {
+	async ready() {
+		const container = document.querySelector<HTMLElement>('.full-height');
+		if (container) fullHeight(container);
+		const $catchMain = document.querySelector(".catch_main");
+		if (!$catchMain) return;
+		const messageArray = messageSplit(this.decryptedMessage);
+		messageArray.forEach((v) => {
+			const $span = document.createElement('span');
+			$span.innerHTML = v;
+			$span.className = 'catch_main_part'
+			$catchMain.appendChild($span);
+		});
+		const $parts = $catchMain.querySelectorAll('.catch_main_part');
+		const partsArray = Array.from($parts);
+		await partsArray.reduce(async (p: Promise<void>, e: Element, i: number) => {
+			await p;
+			e.classList.add("catch_main_part--visible");
+			await timer(400);
+			return p;
+		}, timer(500));
+		await timer(3000);
+		this.finish();
+	}
+}
+
+new App({ telopId: "fxrc44hu" }).init();
